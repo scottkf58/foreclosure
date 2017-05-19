4812ed295d74480119a038de0f0df56ec544d506
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-18 23:15:59 -1000

    on return literal obj

---
+++ foreclosure.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+var steve;
+var stevesLoan;
+var month = 0;
+var monthsUntilEvicted;
+
+function loan () {
+  var account = {
+    borrowed: 550000,
+    balance: 286000,
+    monthlyPayment: 1700,
+    defaulted: 0,
+    defaultsToForeclose: 5,
+    foreclosed: false
+  };
+
+  function missPayment () {
+    account.defaulted++;
+    if(account.defaulted >= account.defaultsToForeclose) {
+      account.foreclosed = true;
+    }
+  }
+  return {
+
+  };
+
+}
