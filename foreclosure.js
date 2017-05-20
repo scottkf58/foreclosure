dfcebc80e4e3665e9d6faff2b978ded94e8e4ca6
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-19 20:37:48 -1000

    payDay/makePayment not being invoked

--- foreclosure.js
+++ foreclosure.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var steve;
 var stevesLoan;
 var month = 0;
@@ -18,16 +17,17 @@ function loan() {
 
   function missPayment() {
     account.defaulted++;
-    if(account.defaulted >= account.defaultsToForeclose) {
+    if (account.defaulted >= account.defaultsToForeclose) {
       account.foreclosed = true;
     }
   }
+
   return {
     getBalance : function() {
       return account.balance;
     },
     receivePayment : function(amount) {
-      if(amount < account.monthlyPayment) {
+      if (amount < account.monthlyPayment) {
         missPayment();
       }
       account.balance -= amount;
@@ -35,11 +35,47 @@ function loan() {
     getMonthlyPayment : function() {
       return account.monthlyPayment;
     },
-    isForclosed : function() {
+    isForeclosed : function() {
       return account.foreclosed;
     }
-
 
   };
 
 }
+
+function borrower(loan) {
+  var account = {
+    monthlyIncome : 1350,
+    funds : 2800,
+    loan : loan
+  };
+
+  return {
+    getFunds : function() {
+      return account.funds;
+    },
+    makePayment : function() {
+      if (account.funds > loan.getMonthlyPayment) {
+        account.funds -= loan.getMonthlyPayment;
+        loan.receivePayment(loan.getMonthlyPayment);
+      } else {
+        loan.receivePayment(account.funds);
+        account.funds = 0;
+      }
+    },
+    payDay : function() {
+      return account.funds += account.monthlyIncome;
+    }
+  };
+}
+
+stevesLoan = loan();
+steve = borrower(stevesLoan);
+
+while (stevesLoan.isForeclosed() === false) {
+  steve.payDay();
+  steve.makePayment();
+  month++;
+}
+
+monthsUntilEvicted = month;
