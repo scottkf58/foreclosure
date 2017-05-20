a70f6804154f89110078ed4e95d24817476480f6
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-19 17:51:17 -1000

    TypeError: Cannot read property 'should' of undefined

--- foreclosure.js
+++ foreclosure.js
@@ -6,23 +6,39 @@ var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
 
-function loan () {
+function loan() {
   var account = {
-    borrowed: 550000,
-    balance: 286000,
-    monthlyPayment: 1700,
-    defaulted: 0,
-    defaultsToForeclose: 5,
-    foreclosed: false
+    borrowed : 550000,
+    balance : 286000,
+    monthlyPayment : 1700,
+    defaulted : 0,
+    defaultsToForeclose : 5,
+    foreclosed : false
   };
 
-  function missPayment () {
+  function missPayment() {
     account.defaulted++;
     if(account.defaulted >= account.defaultsToForeclose) {
       account.foreclosed = true;
     }
   }
   return {
+    getBalance : function() {
+      return account.balance;
+    },
+    receivePayment : function(amount) {
+      if(amount < account.monthlyPayment) {
+        missPayment();
+      }
+      account.balance -= amount;
+    },
+    getMonthlyPayment : function() {
+      return account.monthlyPayment;
+    },
+    isForclosed : function() {
+      return account.foreclosed;
+    }
+
 
   };
 
